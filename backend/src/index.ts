d64b2c29aa8eb8a8cd5fbdb07dd7a4d7a83c21c3
Jarrett0203 <jarrett0203@gmail.com>
2026-09-12 22:09:50 +0800

    feat(backend): Add cors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,11 @@
 import "dotenv/config";
 import express from "express";
 import router from "./routes/index.ts";
+import cors from "cors";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+const allowedOrigins = (process.env.ALLOWED_ORIGINS ?? "").split(",").map((origin) => origin.trim());
 
 app.get("/", (req, res) => {
   res.send("API is running");
@@ -14,3 +16,14 @@ app.listen(PORT, () => {
 });
 
 app.use(router);
+app.use(cors(
+  {
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error(`Origin ${origin} not allowed by CORS`));
+      }
+    }
+  }
+));
